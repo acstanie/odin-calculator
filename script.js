efa7e6e3f4bd37a43f63db4d9ce3724270bfea4d
acstanie <alexhartman95@gmail.com>
2024-01-20 12:31:10 -0400

    Finished work on the interface of the calculator. Still need to fix the display and reduce the size of the calculator afterwards.

---
+++ script.js
@@ -0,0 +1,34 @@
+const btns = document.querySelectorAll(".btns");
+const calcDisplay = document.querySelector(".calc-display");
+
+
+const display = document.createElement("div");
+display.classList.toggle("display");
+calcDisplay.appendChild(display);
+
+const one = document.querySelector(".btn-1");
+const two = document.querySelector(".btn-2");
+const three = document.querySelector(".btn-3");
+const four = document.querySelector(".btn-4");
+const five = document.querySelector(".btn-5");
+const six = document.querySelector(".btn-6");
+const seven = document.querySelector(".btn-7");
+const eight = document.querySelector(".btn-8");
+const nine = document.querySelector(".btn-9");
+const zero = document.querySelector(".btn-0");  
+
+for(let btn of btns) {
+    btn.addEventListener("mouseenter", (e) => {
+        btn.style.cursor = "pointer";
+    });
+}
+
+one.addEventListener("click", (e) => {
+    display.innerHTML += "1";
+    calcDisplay.appendChild(display);
+    
+});
+
+two.addEventListener("click", (e) => {
+    display.textContent += "2";
+});
